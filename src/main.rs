acab64be2eddb4714c832fb227094af327339552
MiguelGss <miguelgsilverios@gmail.com>
2025-06-18 13:26:49 -0300

    melhorias structs

--- src/main.rs
+++ src/main.rs
@@ -3,81 +3,140 @@ const MAP_SIZE_Y: usize = 20;
 const INVERT_ROW_ORIGIN: bool = true;
 const INVERT_COL_ORIGIN: bool = false;
 
-use std::io::{Write, stdin, stdout};
+use iced::widget::button;
 use std::fs::File;
-use macroquad::prelude::*;
+use iced::widget::text;
+use std::io::{Write, stdin, stdout};
 
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "DSquareMapper".to_owned(),
-        fullscreen: false,
-        window_width: 640,
-        window_height: 480,
-        window_resizable: false,
-        ..Default::default()
-    }
+#[repr(u8)]
+#[derive(Copy, Clone)]
+enum ETypeFloor {
+    None = b' ',
+    Floor = b'f',
+    Door = b'd',
+    Trap = b't',
+    Wall = b'w',
 }
 
-struct GlobalSettings<const X: usize, const Y: usize> {
-    map: [[char; X]; Y],
-    squaresMap: [[Rect; X]; Y],
-    mapSizeX: usize,
-    mapSizeY: usize,
-    invertRowOrigin: bool,
-    invertColOrigin: bool
-}
-
-impl Default for GlobalSettings::<20, 20> {
-    fn default() -> Self {
-        GlobalSettings::<20, 20> {
-            map: [[' '; 20]; 20],
-            squaresMap: [[Rect::new(0.0, 0.0, 10.0, 10.0); 20]; 20],
-            mapSizeX: 20,
-            mapSizeY: 20,
-            invertRowOrigin: true,
-            invertColOrigin: false
+impl ETypeFloor {
+    fn from_char(c: char) -> Self {
+        match c {
+            ' ' => ETypeFloor::None,
+            'f' => ETypeFloor::Floor,
+            'd' => ETypeFloor::Door,
+            't' => ETypeFloor::Trap,
+            'w' => ETypeFloor::Wall,
+            _ => ETypeFloor::None
         }
     }
 }
 
-#[macroquad::main(window_conf)]
-async fn main() {
-    let mut global_settings = GlobalSettings::default();
-    // let map: [[char; MAP_SIZE_X]; MAP_SIZE_Y] = [[' '; MAP_SIZE_X]; MAP_SIZE_Y];
-    // start_map_terminal(map);
-    loop {
-        run(&mut global_settings).await;
+impl Into<char> for ETypeFloor {
+    fn into(self) -> char {
+        self as u8 as char
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Cell {
+    x: usize,
+    y: usize,
+    type_floor: ETypeFloor,
+}
+
+impl Cell {
+    fn new(x: usize, y:usize, type_floor: ETypeFloor) -> Self {
+        Self {
+            x,
+            y,
+            type_floor
+        }
     }
 
+    fn update(&mut self, type_floor: ETypeFloor) {
+        self.type_floor = type_floor;
+    }
+}
+
+struct MapTools {
+    type_floor_selected: ETypeFloor,
+}
+
+impl Default for MapTools {
+    fn default() -> Self {
+        MapTools { 
+            type_floor_selected: ETypeFloor::Floor 
+        }
+    }
+}
+
+struct MapData<const X: usize, const Y: usize> {
+    cells: [[Cell; X]; Y],
+    map_size_x: usize,
+    map_size_y: usize,
+    invert_row_origin: bool,
+    invert_col_origin: bool,
+}
+
+impl<const X:usize, const Y:usize> MapData<X,Y> {
+    fn new(cells: [[Cell; X]; Y], map_size_x: usize, map_size_y: usize, invert_row_origin: bool, invert_col_origin: bool) -> Self {
+        Self { cells, map_size_x, map_size_y, invert_row_origin, invert_col_origin }
+    }
+
+    fn update_cell(&mut self, x: usize, y: usize, t: ETypeFloor) {
+        self.cells[x][y].update(t);
+    }
+}
+
+impl Default for MapData<20, 20> {    
+    fn default() -> Self {
+        let cells = std::array::from_fn(|y| {
+            std::array::from_fn(|x| {
+                Cell {
+                    x,
+                    y,
+                    type_floor: ETypeFloor::from_char(' '),
+                }
+            })
+        });
+
+        MapData::<20, 20> {
+            cells,
+            map_size_x: 20,
+            map_size_y: 20,
+            invert_row_origin: true,
+            invert_col_origin: false,
+        }
+    }
+}
+
+fn main() {
+    let md = MapData::default();
+    // let a:&str = &(format!("{}", 'x'));
+    // let counter = text(15);
+    // let increment = button("+");
+    // let decrement = button("-");
+    start_map_terminal(md);
 }
 
 // UI FUNCTIONS
 
-async fn update<const X:usize, const Y:usize>(gs: &mut GlobalSettings<X,Y>) {
+fn update<const X: usize, const Y: usize>(gs: &mut MapData<X, Y>) {
     ()
 }
 
-async fn draw() {
-    clear_background(DARKGREEN);
-    draw_circle(mouse_position().0, mouse_position().1, 10.0, PURPLE);
-    next_frame().await
-}
+fn draw() {}
 
-async fn run<const X:usize, const Y:usize>(gs: &mut GlobalSettings<X,Y>) {
-    update(gs).await;
-    draw().await;
-}
+fn run<const X: usize, const Y: usize>(gs: &mut MapData<X, Y>) {}
 
 // TERMINAL FUNCTIONS
-fn start_map_terminal<const N: usize>(mut map: [[char; N]; N]) {
+fn start_map_terminal<const X: usize, const Y: usize>(mut map: MapData<X, Y>) {
     loop {
-        println!("{}", format_map(map));
+        println!("{}", format_map(map.cells));
         let mut s: String = String::new();
         print!("Pass: (X, Y, Type of floor): ");
         let _ = stdout().flush();
-        stdin()
-            .read_line(&mut s)
-            .expect("Did not enter a correct string");
+        stdin().read_line(&mut s).expect("String error");
         if let Some('\n') = s.chars().next_back() {
             s.pop();
         }
@@ -108,29 +167,34 @@ fn start_map_terminal<const N: usize>(mut map: [[char; N]; N]) {
             get_value_as_usize(&result, 1)
         };
 
-        map[x][y] = result[2].parse::<char>().unwrap();
+        let c:char = result[2].parse::<char>().unwrap();
+        map.update_cell(x, y, ETypeFloor::from_char(c));
         println!("You typed: {}; Last updated coordinates: {}/{}", s, x, y);
 
-        let _ = update_map_file(map);
+        let _ = update_map_file(map.cells);
     }
 }
 
-fn update_map_file<const N: usize>(map: [[char; N]; N]) -> std::io::Result<()> {
+fn update_map_file<const X: usize, const Y: usize>(map: [[Cell; X]; Y]) -> std::io::Result<()> {
     let mut file = File::create("foo.txt")?;
     file.write_all(format_map(map).as_bytes())?;
 
     Ok(())
 }
 
-fn format_map<const N: usize>(map: [[char; N]; N]) -> String{
-    let mut count: i8 = if INVERT_ROW_ORIGIN {19 } else { 0 };
+fn format_map<const X: usize, const Y: usize>(map: [[Cell; X]; Y]) -> String {
+    let mut count: i8 = if INVERT_ROW_ORIGIN { 19 } else { 0 };
     let mut formmated_map: String = "".to_owned();
     for line in map.iter() {
         formmated_map.push_str(&(format!("{:002} - ", count)));
         for cell in line.iter() {
-            formmated_map.push_str(&(format!("[{}]", cell)));
+            formmated_map.push_str(&(format!("[{}]", Into::<char>::into(cell.type_floor))))
         }
-        count = if INVERT_ROW_ORIGIN { count - 1 } else { count + 1 };
+        count = if INVERT_ROW_ORIGIN {
+            count - 1
+        } else {
+            count + 1
+        };
         formmated_map.push_str("\n");
     }
 
